e77a8eb36ba5b8069ca36255672b7649cd695cd4
rahulon12 <rahulon@icloud.com>
2022-03-06 11:04:13 -0500

    Initial Commit

---
+++ FloatingCard/FloatingCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingCardApp.swift
+//  FloatingCard
+//
+//  Created by Rahul Narayanan on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FloatingCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
